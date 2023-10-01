83fb9641cf2ee6f4c91392bab2d12e7b804332f0
soulwax <soulwax-95@protonmail.com>
2023-10-01 03:36:01 +0000

    Refactor code and improve message handling
    Switched from CommonJS to ES6 imports and improved message-handling logic in `app.ts` by adding more checks. Initial bot introduction message logic has been commented out for now. Also refactored `commands/diffuse.ts` to use ES6 imports and return helpful error if message is not found.

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,44 @@
 // initialize discord REST
 
-import Discord, { Channel } from 'discord.js';
-import config from './config';
-import fs from 'fs';
-import path from 'path';
-import server from './setup';
-import axios from 'axios';
-import { AttachmentBuilder } from 'discord.js';
-import dotenv from 'dotenv';
-dotenv.config();
+import config from './config'
+
+import server from './setup'
+import axios from 'axios'
+import { AttachmentBuilder, Message } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 server.client.once('ready', async () => {
-  server.client.once('ready', async () => {
-    console.log(`Logged in as ${server.client.user?.tag}!`);
+  console.log(`Logged in as ${server.client.user?.tag}!`)
 
-    // Code from deployed.js
-    const guilds = await server.client.guilds.fetch();
-    const firstGuild = guilds.first();
-    if (firstGuild) {
-      const guild = firstGuild as unknown as Discord.Guild;
-      const channels = await guild.channels.fetch();
-      const generalChannel = channels.find((channel) => channel?.id === '1135408229329944579') as Discord.TextChannel;
-      if (generalChannel) {
-        await generalChannel.send(
-          [
-            `Hey there! Thanks for installing this Discord bot.`,
-            `Try mentioning me in a message like this: <@!${server.client.user?.id}>`,
-            '',
-            `You can support our codebase here:`,
-            `https://github.com/soulwax/Midoridan`,
-          ].join('\n'),
-        );
-      }
-    }
-  });
-});
+  // Code from deployed.js
+  // const guilds = await server.client.guilds.fetch();
+  // const firstGuild = guilds.first();
+  // if (firstGuild) {
+  //   const guild = firstGuild as unknown as Discord.Guild;
+  //   const channels = await guild.channels.fetch();
+  //   const generalChannel: TextChannel = channels.find(
+  //     (channel) => channel?.id === '1135408229329944579',
+  //   ) as TextChannel;
+  //   // if (generalChannel) {
+  //   //   await generalChannel.send(
+  //   //     [
+  //   //       `Hey there! Thanks for installing this Discord bot.`,
+  //   //       `Try mentioning me in a message like this: <@!${server.client.user?.id}>`,
+  //   //       '',
+  //   //       `You can support our codebase here:`,
+  //   //       `https://github.com/soulwax/Midoridan`,
+  //   //     ].join('\n'),
+  //   //   );
+  //   // }
+  // }
+})
 
-server.client.on('messageCreate', async (message) => {
+server.client.on('messageCreate', async (message: Message) => {
+  const wasMentioned: boolean = message.mentions.has(server.client.user?.id ?? '')
+  console.log(`wasMentioned: ${wasMentioned}`)
+  const mentionedHas = server.client.user?.toString() ?? ''
+  console.log(`mentionedHas: ${mentionedHas}`)
   if (message.content.startsWith('hey')) {
     await message.reply(
       [
@@ -44,28 +46,27 @@ server.client.on('messageCreate', async (message) => {
         '',
         `This is an example handler that responds to messages starting with "hey"`,
       ].join('\n'),
-    );
+    )
   }
-  if (message.mentions.has(server.client.user?.toString() ?? '')) {
+  if (message.mentions.has(server.client.user?.toString() ?? '') || wasMentioned) {
     try {
       const response = await axios.get('https://dreamstudio.ai/api/endpoint', {
         responseType: 'arraybuffer', // To handle binary data
-      });
-
+      })
       // Convert the binary data to a Discord attachment
-      const attachment = new AttachmentBuilder(response.data, { name: 'image.png' }); // Assuming the image is a PNG, adjust the filename accordingly
-      console.log(`=== data from dreamstudio.ai ===`);
-      console.dir(response.data);
+      const attachment = new AttachmentBuilder(response.data, { name: 'image.png' }) // Assuming the image is a PNG, adjust the filename accordingly
+      console.log(`=== data from dreamstudio.ai ===`)
+      console.dir(response.data)
       // Send the image to the channel, mentioning the user and reiterating their prompt
       await message.channel.send({
         content: `<@${message.author.id}> Generated image for: "${message.content}"`,
         files: [attachment],
-      });
+      })
     } catch (error) {
-      console.error('Error fetching image from dreamstudio.ai:', error);
-      await message.reply('Sorry, I encountered an error while fetching the image.');
+      console.error('Error fetching image from dreamstudio.ai:', error)
+      await message.reply('Sorry, I encountered an error while fetching the image.')
     }
   }
-});
+})
 
-server.client.login(config.token); // Replace with your bot token
+server.client.login(config.token) // Replace with your bot token
--- src/commands/diffuse.ts
+++ src/commands/diffuse.ts
@@ -1,22 +1,21 @@
 // src/commands/diffuse.js
-
-const { MessageEmbed } = require("discord.js");
-const { get } = require("axios");
-const { parse } = require("node-html-parser");
+import { EmbedBuilder } from 'discord.js'
+import get from 'axios'
+import { parse } from 'node-html-parser'
 
 module.exports = {
-  name: "diffuse",
-  description: "Diffuse a message",
-  async execute(msg) {
-    const url = "https://www.stablenet.org/diffuse.php";
-    const params = { message: msg };
-    const { data } = await get(url, { params });
-    const root = parse(data);
-    const embed = new MessageEmbed()
-      .setColor("#0099ff")
-      .setTitle("Stable Diffusion v2.1")
+  name: 'diffuse',
+  description: 'Diffuse a message',
+  async execute(msg: string) {
+    const url = 'https://www.stablenet.org/diffuse.php'
+    const params = { message: msg }
+    const { data } = await get(url, { params })
+    const root = parse(data)
+    const embed = new EmbedBuilder()
+      .setColor('#0099ff')
+      .setTitle('Stable Diffusion v2.1')
       .setURL(url)
-      .setDescription(root.querySelector("div").text);
-    return embed;
+      .setDescription(root.querySelector('div')?.text || 'No message found')
+    return embed
   },
-};
+}
